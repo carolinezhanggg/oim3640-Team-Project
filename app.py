a9f38244cbc09dda1c8928ef473e607b7d352c4a
carolinezhanggg <xzhang9@babson.edu>
2022-04-27 00:08:38 -0400

    Finish Final Project

---
+++ app.py
@@ -0,0 +1,27 @@
+"""
+application that can find the nearest MBTA and shows whether it is wheelchair accessible using Flask
+"""
+
+from flask import Flask, render_template, request
+from helper import get_temp, get_events
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def show_weather():
+    if request.method == "POST":
+        city_name = request.form['city']
+        country_code = request.form['countrycode']
+        state_code = request.form['statecode']
+        number = request.form.get('How many number of events would you want to be listed?',type = int)
+        temperature = get_temp(city_name,country_code)
+        events = get_events(city_name,state_code,number)
+        return render_template('weather-result.html', city=city_name, temp=temperature, events=events)
+    else:
+        return render_template("weather.html")
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ helper.py
@@ -0,0 +1,63 @@
+import urllib.request
+import json
+from pprint import pprint
+
+# API KEYS
+from config import WEATHER_API_KEY, CONCERT_API_KEY
+
+# Useful URLs
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+CONCERT_URL = "https://app.ticketmaster.com/discovery/v2/events.json"
+
+
+
+def get_json(url):
+    """
+    Given a properly formatted URL for a JSON web API request, return
+    a Python JSON object containing the response to that request.
+    """
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
+
+def get_temp(city,country_code):
+    """
+    Given a city and country code, return the current temperature of the given place. 
+    """
+    url = WEATHER_URL + f'?q={city},{country_code}&APPID={WEATHER_API_KEY}'
+    response_data = get_json(url)
+    temp = response_data['main']['temp'] - 273.15
+    return round(temp,0)
+
+def get_events(city,state_code,number):
+    """
+    Given city and state code, return the recent concert's date, name, picture, and url hold in the given place.
+    """
+    url = CONCERT_URL + f'?apikey={CONCERT_API_KEY}&city={city}&stateCode={state_code}'
+    response_data = get_json(url)
+    events = []
+    i = 0
+    for i in range (number):
+        dic = {}
+        dic['name'] = response_data['_embedded']['events'][i]['name']
+        dic['pic'] = response_data['_embedded']['events'][i]['images'][0]['url']
+        dic['date'] = response_data['_embedded']['events'][i]['dates']['start']['localDate']
+        dic['link'] = response_data['_embedded']['events'][i]['url']
+        events.append(dic)
+
+    return events
+
+
+def main():
+    city = 'Boston'
+    country_code = 'US'
+    state_code = 'MA'
+    number = 3
+    print(get_temp(city,country_code))
+    print(get_events(city,state_code,number))
+
+
+
+if __name__ == '__main__':
+    main()
